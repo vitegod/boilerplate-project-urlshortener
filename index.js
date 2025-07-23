b11598796fa3583888323a80fb32171ef815bd75
Viet Nguyen Hoang <nhv1312004@gmail.com>
2025-07-23 23:34:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const urlParser = require("url")
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+let urlDB = []
+let id = 1;
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.post("/api/shorturl", (req, res) => {
+  const originURL = req.body.url;
+  let hostname;
+  try {
+    hostname = urlParser.parse(originURL).hostname;
+  } catch (error) {
+    return res.json({
+      error: "Invalid URL"
+    })
+  }
+
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      return res.json({
+        error: "Invalid URL"
+      })
+    }
+    else {
+      const shorten = id++;
+      urlDB[shorten] = originURL;
+      res.json({ originURL, shorten })
+    }
+  })
+})
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const shorten = req.params.short_url;
+  const originURL = urlDB[shorten]
+  if (originURL) {
+    res.redirect(originURL)
+  }
+  else {
+    res.json({
+      error: "Invalid URL"
+    })
+  }
+})
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
